600b92ee8d43041f462d33a6bc7e42ba771ebe97
sarp <sarpderinsu@hotmail.nl>
2020-10-13 18:19:58 +0200

    sdafsadf

--- config/oauth-drivers.php
+++ config/oauth-drivers.php
@@ -2,13 +2,13 @@
 
 return [
     'gitlab' => [
-        'urlAuthorize' => '/oauth/authorize',
-        'urlAccessToken' => '/oauth/token',
-        'urlResourceOwnerDetails' => '/api/v4/user'
+        'urlAuthorize' => 'oauth/authorize',
+        'urlAccessToken' => 'oauth/token',
+        'urlResourceOwnerDetails' => 'api/v4/user'
     ],
     'bitbucket' => [
-        'urlAuthorize' => '/1.0/oauth/authenticate',
-        'urlAccessToken' => '/1.0/oauth/access_token',
-        'urlResourceOwnerDetails' => '/2.0/user'
+        'urlAuthorize' => '1.0/oauth/authenticate',
+        'urlAccessToken' => '1.0/oauth/access_token',
+        'urlResourceOwnerDetails' => '2.0/user'
     ]
 ];
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,7 +6,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'../config/oauth-drivers.php', 'oauth-drivers');
-        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
+        $this->mergeConfigFrom(__DIR__.'/../config/oauth-drivers.php', 'oauth-drivers');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
